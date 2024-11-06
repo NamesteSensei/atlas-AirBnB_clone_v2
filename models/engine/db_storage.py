60416671b260e750d2aa1668b54f77df887c44ff
namestesensei <christopherwright435@gmail.com>
2024-11-05 23:26:49 -0600

    Fix session management in DBStorage: use scoped_session for proper session removal in close()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,64 +1,69 @@
 #!/usr/bin/python3
-"""DBStorage class for storage management with MySQL database"""
+"""Database storage engine"""
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
 from models.state import State
 from models.city import City
-from os import getenv
+import os
 
 class DBStorage:
-    """Database storage engine for MySQL database"""
+    """Database Storage"""
 
     __engine = None
     __session = None
 
     def __init__(self):
-        """Initialize the database engine and session"""
+        """Initialize the database storage"""
+        user = os.getenv("HBNB_MYSQL_USER")
+        password = os.getenv("HBNB_MYSQL_PWD")
+        host = os.getenv("HBNB_MYSQL_HOST")
+        database = os.getenv("HBNB_MYSQL_DB")
         self.__engine = create_engine(
-            f"mysql+mysqldb://{getenv('HBNB_MYSQL_USER')}:{getenv('HBNB_MYSQL_PWD')}@"
-            f"{getenv('HBNB_MYSQL_HOST')}/{getenv('HBNB_MYSQL_DB')}",
+            f"mysql+mysqldb://{user}:{password}@{host}/{database}",
             pool_pre_ping=True
         )
 
-        if getenv("HBNB_ENV") == "test":
-            Base.metadata.drop_all(bind=self.__engine)
+        # Drop all tables if HBNB_ENV is test
+        if os.getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query all objects by class, or all classes if none specified"""
+        """Query on the current database session for all objects"""
         objects = {}
         if cls:
-            for obj in self.__session.query(cls).all():
+            objs = self.__session.query(cls).all()
+            for obj in objs:
                 key = f"{obj.__class__.__name__}.{obj.id}"
                 objects[key] = obj
         else:
-            for cls in [State, City]:
-                for obj in self.__session.query(cls).all():
+            for cls_name in [State, City]:  # add other classes as needed
+                objs = self.__session.query(cls_name).all()
+                for obj in objs:
                     key = f"{obj.__class__.__name__}.{obj.id}"
                     objects[key] = obj
         return objects
 
     def new(self, obj):
-        """Add a new object to the database session"""
+        """Add the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """Commit all changes to the database"""
+        """Commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete an object from the database if it exists"""
+        """Delete obj from the current database session"""
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create tables in the database and initialize the session"""
-        from models.base_model import Base
+        """Reload data from the database"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session_factory)
 
     def close(self):
-        """Close the session"""
-        self.__session.remove()
+        """Close the current session"""
+        self.__session.remove()  # Correct use of remove with scoped_session
